5f4d35d4f172499df1dfad213613789540ce495d
Irving Zamora <irvingzamora28@gmail.com>
2024-01-04 06:38:45 -0600

    feat: Refactor CLI interface for enhanced readability and add default file_path - Standardize quotation marks for string literals - Add default value for file_path in user input to the valid test file

--- lesson_generator/cli_interface.py
+++ lesson_generator/cli_interface.py
@@ -2,7 +2,8 @@ import questionary
 import os
 
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def is_valid_file_path(file_path):
     if os.path.isfile(file_path):
@@ -10,7 +11,8 @@ def is_valid_file_path(file_path):
     elif os.path.isfile(os.path.join(ROOT_DIR, file_path)):
         return True
     else:
-        return 'Please enter a valid file path.'
+        return "Please enter a valid file path."
+
 
 def is_valid_directory(val):
     if os.path.isdir(val):
@@ -18,39 +20,39 @@ def is_valid_directory(val):
     elif os.path.isdir(os.path.join(ROOT_DIR, val)):
         return True
     else:
-        return 'Please enter a valid directory.'
+        return "Please enter a valid directory."
+
 
 questions = [
     {
-        'type': 'list',
-        'name': 'action',
-        'message': 'What do you want to do?',
-        'choices': [
-            'Generate lessons',
-            'Exit'
-        ]
+        "type": "list",
+        "name": "action",
+        "message": "What do you want to do?",
+        "choices": ["Generate lessons", "Exit"],
     },
     {
-        'type': 'input',
-        'name': 'file_path',
-        'message': 'Enter the JSON file path:',
-        'when': lambda answers: answers['action'] == 'Generate lessons',
-        'validate': is_valid_file_path
+        "type": "input",
+        "name": "file_path",
+        "message": "Enter the JSON file path:",
+        "when": lambda answers: answers["action"] == "Generate lessons",
+        "validate": is_valid_file_path,
+        "default": lambda _: "./tests/valid_input.json",
     },
     {
-        'type': 'input',
-        'name': 'output_directory',
-        'message': 'Enter the directory where you want the lessons to be saved:',
-        'when': lambda answers: answers['action'] == 'Generate lessons',
-        'validate': is_valid_directory,
-        'default': lambda _: './sample_output'
-    }
+        "type": "input",
+        "name": "output_directory",
+        "message": "Enter the directory where you want the lessons to be saved:",
+        "when": lambda answers: answers["action"] == "Generate lessons",
+        "validate": is_valid_directory,
+        "default": lambda _: "./sample_output",
+    },
 ]
+
 
 def main(generate_lessons_func):
     answers = questionary.prompt(questions)
-    
-    if answers['action'] == 'Generate lessons':
-        generate_lessons_func(answers['file_path'], answers['output_directory'])
+
+    if answers["action"] == "Generate lessons":
+        generate_lessons_func(answers["file_path"], answers["output_directory"])
     else:
         print("Exiting...")
